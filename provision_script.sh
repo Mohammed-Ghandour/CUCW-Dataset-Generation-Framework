7e8b8e6326fe225424a450ba3ca5cd390eb611df
Mohammed-Ghandour <eng.mohammedghandour@gmail.com>
2025-11-27 21:36:59 +0200

    Update provision_script.sh

--- provision_script.sh
+++ provision_script.sh
@@ -20,7 +20,7 @@ echo "Suites: \$(. /etc/os-release && echo \"\${UBUNTU_CODENAME:-\$VERSION_CODEN
 echo "Components: stable" >> /etc/apt/sources.list.d/docker.sources
 echo "Signed-By: /etc/apt/keyrings/docker.asc"  >> /etc/apt/sources.list.d/docker.sources
 
-apt update && apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt update && apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Set up any additional services
 echo "[program:docker]" > /etc/supervisor/conf.d/docker.conf
@@ -29,9 +29,6 @@ echo "autostart=true" >> /etc/supervisor/conf.d/docker.conf
 echo "autorestart=true" >> /etc/supervisor/conf.d/docker.conf
 echo "stderr_logfile=/var/log/docker.err.log" >> /etc/supervisor/conf.d/docker.conf
 echo "stdout_logfile=/var/log/docker.out.log" >> /etc/supervisor/conf.d/docker.conf
-
-echo "my-supervisor-wrapper" > /opt/supervisor-scripts/my-application.sh
-chmod +x /opt/supervisor-scripts/my-application.sh
 
 # Reload Supervisor
 supervisorctl reload
