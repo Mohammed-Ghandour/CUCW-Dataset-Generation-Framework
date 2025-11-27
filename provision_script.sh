02dbc338375505d56f76e0af3c00a4f4027f2789
Mohammed-Ghandour <eng.mohammedghandour@gmail.com>
2025-11-27 21:01:43 +0200

    Create provision_script.sh

---
+++ provision_script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#
+cd /workspace/
+# Cause the script to exit on failure.
+set -eo pipefail
+
+# Activate the main virtual environment
+. /venv/main/bin/activate
+
+## Install Docker
+apt update && apt install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+echo "Types: deb" > /etc/apt/sources.list.d/docker.sources
+echo "URIs: https://download.docker.com/linux/ubuntu" >> /etc/apt/sources.list.d/docker.sources
+echo "Suites: \$(. /etc/os-release && echo \"\${UBUNTU_CODENAME:-\$VERSION_CODENAME}\")" >> /etc/apt/sources.list.d/docker.sources
+echo "Components: stable" >> /etc/apt/sources.list.d/docker.sources
+echo "Signed-By: /etc/apt/keyrings/docker.asc"  >> /etc/apt/sources.list.d/docker.sources
+
+apt update && apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Set up any additional services
+echo "[program:docker]" > /etc/supervisor/conf.d/docker.conf
+echo "command=/usr/bin/dockerd -H unix:///var/run/docker.sock" >> /etc/supervisor/conf.d/docker.conf
+echo "autostart=true" >> /etc/supervisor/conf.d/docker.conf
+echo "autorestart=true" >> /etc/supervisor/conf.d/docker.conf
+echo "stderr_logfile=/var/log/docker.err.log" >> /etc/supervisor/conf.d/docker.conf
+echo "stdout_logfile=/var/log/docker.out.log" >> /etc/supervisor/conf.d/docker.conf
+
+echo "my-supervisor-wrapper" > /opt/supervisor-scripts/my-application.sh
+chmod +x /opt/supervisor-scripts/my-application.sh
+
+# Reload Supervisor
+supervisorctl reload
+supervisorctl update
+
+# Start Docker
+supervisorctl start docker
