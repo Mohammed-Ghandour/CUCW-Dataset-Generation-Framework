1b8e1f5456b851f2d9d8fb690de67a328a833421
Mohammed-Ghandour <eng.mohammedghandour@gmail.com>
2025-11-29 01:18:17 +0200

    Update machine_setup.sh

--- machine_setup.sh
+++ machine_setup.sh
@@ -7,7 +7,7 @@ apt update && apt install -y apt-transport-https ca-certificates gnupg curl cuda
 # Download google-cloud-cli
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-apt update && apt install google-cloud-cli
+apt update && apt install -y google-cloud-cli
 
 # Configure gcloud
 gcloud init
