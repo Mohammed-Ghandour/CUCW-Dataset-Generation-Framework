0ff851137b593a74626b21a20f3859af89345583
Mohammed-Ghandour <eng.mohammedghandour@gmail.com>
2025-11-30 00:36:09 +0200

    Update machine_setup.sh

--- machine_setup.sh
+++ machine_setup.sh
@@ -27,23 +27,20 @@
 # Install the toolkit
 #apt update && apt install -y nvidia-docker2
 
-apt update && apt install -y zip python3.7 python3.7-dev python3.7-venv
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
+#apt update && apt install -y zip python3.7 python3.7-dev python3.7-venv
+#update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
+#update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
+#update-alternatives --config python3
 
-update-alternatives --config python3
-
-python3.7 -m pip install --upgrade pip
-python3.7 -m pip install jupyter_core
-python3.7 -m pip install notebook jupyterlab
+#python3.7 -m pip install --upgrade pip
+#python3.7 -m pip install jupyter_core
+#python3.7 -m pip install notebook jupyterlab
 
 # Clone TF3D
 git clone https://github.com/google-research/google-research.git
 
 # Revert to the tested version
 cd google-research; git reset --hard 512a91d1c;
-
-find /usr -name "libcudart*"
 
 # Download Waymo Night Dataset
 curl -L -o waymo-night-training-v2.zip https://www.kaggle.com/api/v1/datasets/download/mohammedosama/waymo-night-training-v2
